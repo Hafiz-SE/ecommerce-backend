bf3bbf104e915bc5408188f2efbadd5d9a23c453
M. Hafizur Rahman <h.rahman1995@outlook.com>
2025-07-11 17:48:29 +0600

    init: added initial dependencies

---
+++ src/main/java/com/wsd/ecommerce/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.wsd.ecommerce.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+import static com.wsd.ecommerce.constant.SwaggerConstants.*;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI config(@Value("${server.servlet.context-path:}") String contextPath) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(API_TITLE)
+                        .version(API_VERSION)
+                        .description(API_DESCRIPTION))
+                .servers(List.of(
+                        new Server().url(contextPath.isEmpty() ? "/" : contextPath)));
+    }
+}
---
+++ src/main/java/com/wsd/ecommerce/constant/SwaggerConstants.java
@@ -0,0 +1,10 @@
+package com.wsd.ecommerce.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SwaggerConstants {
+    public static final String API_VERSION = "v1";
+    public static final String API_TITLE = "WSD Ecommerce";
+    public static final String API_DESCRIPTION = "WSD Ecommerce API";
+}
